d220619edd3e62a655c48a4b76e6359c3de1b979
JheeKim <jheekim992@gmail.com>
2024-06-19 14:35:01 +0900

    결과 페이지 마크업 중

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Loading from "./components/Loading";
+import Result from "./Pages/Result";
 
 function App() {
   const [loading, setLoading] = useState(false);
   return (
     <>
-      <h1>What is your ideal type?</h1>
+      {/* <h1>What is your ideal type?</h1> */}
       <div className="App">{loading ? <Loading></Loading> : <></>}</div>
+      <Result />
     </>
   );
 }
