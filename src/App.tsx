ac60fa3cfa99e5b8256764a0f5d4752c78fc47bd
joshyeom <jhyeom95@daum.net>
2024-06-19 21:04:24 +0900

    Feat: Mypage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SignUp from "./Pages/SignUp";
 import Main from "./Pages/Main";
 import Question from "./Pages/Question";
 import Home from "./Pages/Home";
+import MyPage from "./Pages/MyPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/question" element={<Question />} />
           <Route path="/home" element={<Home/>}></Route>
+          <Route path="/mypage" element={<MyPage/>}></Route>
         </Routes>
       </Router>
 
