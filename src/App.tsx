04ba87094accb4a789c39d1234e11d222f0773a8
joshyeom <jhyeom95@daum.net>
2024-06-19 20:42:06 +0900

    Merge branch 'dev' of https://github.com/joshyeom/idealType into dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Loading from "./components/Loading";
+import Loading from "./Components/Loading";
 import SignUp from "./Pages/SignUp";
 import Main from "./Pages/Main";
 import Question from "./Pages/Question";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
-
       <Router>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/question" element={<Question />} />
+          <Route path="/home" element={<Home/>}></Route>
         </Routes>
       </Router>
 
