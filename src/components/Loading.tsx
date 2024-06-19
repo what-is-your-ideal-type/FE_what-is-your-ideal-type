0dce026793b7d36228975c0661e45e7b25396f6b
joshyeom <jhyeom95@daum.net>
2024-06-19 21:04:10 +0900

    Update: 정적 파일은 public 폴더에 넣어야 하므로 파일 재조정

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import spin from "../assets/images/spin.gif";
 
 export default function Loading() {
   return (
     <div>
-      <img src={spin} alt="loading" width="10%" />
+      <img src="/images/spin.gif" alt="loading" width="10%" />
       <h2>이상형을 찾고 있어요!</h2>
       <h3>잠시만 기다려주세요.</h3>
     </div>
