f56218c5f282730cd4e65f0d3efc0cdd2a123122
sennydayk <seyeon981217@gmail.com>
2024-06-26 14:39:18 +0900

    feat: survey ui, 페이징 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Loading from "./Components/Loading";
 import SignUp from "./Pages/SignUp";
 import Main from "./Pages/Main";
-import Question from "./Pages/Question";
+import Question from "./Pages/Survey";
 import Home from "./Pages/Home";
 import MyPage from "./Pages/MyPage";
 
@@ -15,11 +14,10 @@ function App() {
           <Route path="/" element={<Main />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/question" element={<Question />} />
-          <Route path="/home" element={<Home/>}></Route>
-          <Route path="/mypage" element={<MyPage/>}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/mypage" element={<MyPage />}></Route>
         </Routes>
       </Router>
-
     </>
   );
 }
