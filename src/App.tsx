7f6aa6ca378bca9e95c98b05c384f21d1578de7c
sennydayk <seyeon981217@gmail.com>
2024-06-26 18:52:19 +0900

    Fix: Survey 버튼 수정, ui 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./pages/SignUp";
-import Main from "./pages/Main";
-import Question from "./pages/Survey";
+import Survey from "./pages/Survey";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
+import Result from "./pages/Result";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Main />} />
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/question" element={<Question />} />
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/mypage" element={<MyPage />}></Route>
+          <Route path="/survey" element={<Survey />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/mypage" element={<MyPage />} />
         </Routes>
       </Router>
     </>
