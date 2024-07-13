9ae5ec41bc2072acfaa9433d98fbc6d4c3bc2134
joshyeom <jhyeom95@daum.net>
2024-07-13 14:23:07 +0900

    image sizes 추가

--- src/Pages/Result.tsx
+++ src/Pages/Result.tsx
@@ -54,6 +54,12 @@ const Result = () => {
               ${imageUrl}.webp 800w,
               ${imageUrl}.webp 1024w
             `}
+            sizes="(max-width: 320px) 280px, 
+              (max-width: 480px) 440px, 
+              (max-width: 600px) 500px, 
+              (max-width: 800px) 760px, 
+              1024px
+            "
             loading="lazy"
           />
         </div>
