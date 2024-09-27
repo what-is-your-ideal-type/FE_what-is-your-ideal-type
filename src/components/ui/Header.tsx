ca840be06c4120a04f9ab85778d5b3d087f6718c
sennydayk <seyeon981217@gmail.com>
2024-09-27 09:11:27 +0900

    Feat: header 로그인여부 판별로직, 로그아웃 추가 / Fix: 페이지 header 추가

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -4,6 +4,9 @@ import { useMediaQuery } from "usehooks-ts";
 import { Button } from "./Button";
 import { NavigateFunction, useNavigate } from "react-router-dom";
 import { Text } from "./Text";
+import { useAuth } from "../../contexts/AuthContext";
+import { auth } from "../../firebase";
+import { signOut } from "firebase/auth";
 
 const StyledHeader = styled.header`
   width: 100%;
@@ -12,7 +15,7 @@ const StyledHeader = styled.header`
   padding: 0 50px;
   align-items: center;
   top: 0;
-  
+
   @media (max-width: 768px) {
     justify-content: flex-start;
     padding: 0 10px;
@@ -20,34 +23,80 @@ const StyledHeader = styled.header`
 `;
 
 interface HeaderComponentType {
-    navigate: NavigateFunction;
+  navigate: NavigateFunction;
 }
-  
 
 const MobileComponent = ({ navigate }: HeaderComponentType) => (
-  <Button bgColor="white" width="auto" onClick={() => navigate('/')}>
-    <img src="/images/chervon_left.png" alt="chervon_left.png"/>
+  <Button bgColor="white" width="auto" onClick={() => navigate("/")}>
+    <img src="/images/chervon_left.png" alt="chervon_left.png" />
   </Button>
 );
 
-const DeskTopComponent = ({ navigate }: HeaderComponentType) => (
-  <>
-    <Button bgColor="white" label="홈" width="auto" onClick={() => navigate('/')}>
-      <Text fontSize="lg" fontWeight="bold" color="black">홈</Text>
-    </Button>
-    <Button  bgColor="white" label="로그인" width="auto" onClick={() => navigate('signin')}>
-      <Text fontSize="lg" fontWeight="bold" color="black">로그인</Text>
-    </Button>
-  </>
-);
+const DeskTopComponent = ({ navigate }: HeaderComponentType) => {
+  const { currentUser } = useAuth();
+
+  const handleLogout = async () => {
+    const confirmation = window.confirm("로그아웃 하시겠습니까?");
+    if (confirmation) {
+      try {
+        await signOut(auth);
+        navigate("/");
+      } catch (error) {
+        console.error("Logout failed", error);
+      }
+    }
+  };
+
+  return (
+    <>
+      <Button
+        bgColor="white"
+        label="홈"
+        width="auto"
+        onClick={() => navigate("/")}
+      >
+        <Text fontSize="lg" fontWeight="bold" color="black">
+          홈
+        </Text>
+      </Button>
+      {currentUser ? (
+        <Button
+          bgColor="white"
+          label="로그아웃"
+          width="auto"
+          onClick={handleLogout}
+        >
+          <Text fontSize="lg" fontWeight="bold" color="black">
+            로그아웃
+          </Text>
+        </Button>
+      ) : (
+        <Button
+          bgColor="white"
+          label="로그인"
+          width="auto"
+          onClick={() => navigate("signin")}
+        >
+          <Text fontSize="lg" fontWeight="bold" color="black">
+            로그인
+          </Text>
+        </Button>
+      )}
+    </>
+  );
+};
 
 export const Header = () => {
-  const isMobile = useMediaQuery('(max-width: 768px)');
+  const isMobile = useMediaQuery("(max-width: 768px)");
   const navigate = useNavigate();
 
   return (
-      <StyledHeader>
-        {isMobile ? <MobileComponent navigate={navigate} /> : <DeskTopComponent navigate={navigate} />}
-      </StyledHeader>
+    <StyledHeader>
+      {isMobile ? (
+        <MobileComponent navigate={navigate} />
+      ) : (
+        <DeskTopComponent navigate={navigate} />
+      )}
+    </StyledHeader>
   );
 };
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from "react";
-import {
-  doc,
-  getDoc,
-} from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { useAuth } from "../contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 import NavigateToSurvey from "../components/functional/NavigateToSurvey";
@@ -12,6 +9,7 @@ import { GridBox } from "../styles/GridBox";
 import { Card } from "../styles/styled";
 import { Main } from "../components/ui/Main";
 import { db } from "../firebase";
+import { Header } from "../components/ui/Header";
 
 interface CardData {
   imageUrl: string;
@@ -29,18 +27,17 @@ const MyPage = () => {
   const navigate = useNavigate();
   useEffect(() => {
     const getCardsData = async () => {
-      try{
+      try {
         if (currentUser) {
           const uid = currentUser.uid;
-
 
           const docRef = doc(db, "users", uid);
           const usersSnapShot = await getDoc(docRef);
           const userData = usersSnapShot.data();
-          
-          if(!userData) return
 
-          const postList = userData.postList
+          if (!userData) return;
+
+          const postList = userData.postList;
 
           const fetchCardsData = async () => {
             try {
@@ -49,9 +46,9 @@ const MyPage = () => {
                   const postRef = doc(db, "posts", num);
                   const postSnapShot = await getDoc(postRef);
                   const data = postSnapShot.data();
-          
+
                   if (!data) return null; // 데이터를 가져오지 못한 경우 null 반환
-          
+
                   return {
                     imageUrl: data.imageUrl,
                     createdAt: data.createdAt.toDate(), // Timestamp를 Date 객체로 변환
@@ -59,64 +56,78 @@ const MyPage = () => {
                     hashTags: data.hashTags,
                     resultUrl: `${window.location.href}/result/${data.id}`,
                     profile: data.profile,
-                    id: data.id
+                    id: data.id,
                   };
-                })
+                }),
               );
-          
+
               // null 값이 있을 수 있으므로 필터링
               setCards(fetchedCards);
             } catch (error) {
               console.error("Error fetching cards data:", error);
             }
           };
-          fetchCardsData()
+          fetchCardsData();
         } else {
           alert("로그인이 필요한 서비스입니다. 홈으로 이동합니다.");
           navigate("/");
         }
-      }catch(error){
-        console.error(error)
+      } catch (error) {
+        console.error(error);
       }
-    }
-    getCardsData()
+    };
+    getCardsData();
   }, [currentUser]);
 
   return (
-    <Main>
-      <FlexBox direction="column">
-        <Text fontSize="xl" fontWeight="bold" style={{padding: "1rem 0"}}>
-          {currentUser?.email}님의 이상형 리스트 입니다.
-        </Text>
-        <NavigateToSurvey label="새로운 이상형 찾기" />
-        <GridBox>
-          {cards.map((card, index) => (
-            <Card
-              key={index}
-              onClick={() =>
-                navigate(`/result/${card.id}`)
-              }
-            >
-              <FlexBox direction="column" gap="16px">
-                <img
-                  src={card.imageUrl}
-                  alt={card.fileName}
-                  style={{ width: "100%", height: "auto", borderRadius: "0.375rem 0.375rem 0 0"}}
-                />
+    <>
+      <Header></Header>
+      <Main>
+        <FlexBox direction="column">
+          <Text fontSize="xl" fontWeight="bold" style={{ padding: "1rem 0" }}>
+            {currentUser?.email}님의 이상형 리스트 입니다.
+          </Text>
+          <NavigateToSurvey label="새로운 이상형 찾기" />
+          <GridBox>
+            {cards.map((card, index) => (
+              <Card key={index} onClick={() => navigate(`/result/${card.id}`)}>
                 <FlexBox direction="column" gap="16px">
-                  <FlexBox direction="column">
+                  <img
+                    src={card.imageUrl}
+                    alt={card.fileName}
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                      borderRadius: "0.375rem 0.375rem 0 0",
+                    }}
+                  />
+                  <FlexBox direction="column" gap="16px">
+                    <FlexBox direction="column">
                       {card.hashTags?.map((hashTag, index) => (
-                        <span key={index} style={{padding: "6px 12px", backgroundColor: "rgba(112, 110, 244, 0.3)", display: "inline-block", borderRadius: "34px"}}>#{hashTag}</span>
+                        <span
+                          key={index}
+                          style={{
+                            padding: "6px 12px",
+                            backgroundColor: "rgba(112, 110, 244, 0.3)",
+                            display: "inline-block",
+                            borderRadius: "34px",
+                          }}
+                        >
+                          #{hashTag}
+                        </span>
                       ))}
+                    </FlexBox>
+                    <Text fontWeight="bold">
+                      {card.createdAt.toLocaleDateString()}
+                    </Text>
                   </FlexBox>
-                  <Text fontWeight="bold">{card.createdAt.toLocaleDateString()}</Text>
                 </FlexBox>
-              </FlexBox>
-            </Card>
-          ))}
-        </GridBox>
-      </FlexBox>
-    </Main>
+              </Card>
+            ))}
+          </GridBox>
+        </FlexBox>
+      </Main>
+    </>
   );
 };
 
