b1bcf3ce72a2aef8391133d93eee0a5f854bc740
sennydayk <seyeon981217@gmail.com>
2024-10-18 18:55:04 +0900

    feat: 카카오 로그인 기능

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './reset.css'
-import './index.css'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./reset.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
+
+// if (window.Kakao && !window.Kakao.isInitialized()) {
+//   window.Kakao.init(process.env.REACT_APP_KAKAO_API_KEY);
+//   console.log("Kakao SDK initialized");
+// }
+
 root.render(
   // <React.StrictMode>
-    <App />
+  <App />,
   // </React.StrictMode>
 );
 
