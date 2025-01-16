95d38e6cd869aced4be9e08bf6ed0b3cf94e62c9
sennydayk <seyeon981217@gmail.com>
2025-01-17 00:50:34 +0900

    Fix: vercel.json 조정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   server: {
     open: true,
