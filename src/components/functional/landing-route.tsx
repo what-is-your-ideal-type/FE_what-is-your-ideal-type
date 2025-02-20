72f25c39d030c271ca14f09749b6a2ef4efa5e2b
Kim Seyeon <141703065+sennydayk@users.noreply.github.com>
2025-02-20 21:51:14 +0900

    Dev (#183)
    * test: 디버깅 코드 추가
    
    * Fix: vercel.json 조정
    
    * Fix: convert-to-webp 요청 url 수정
    
    * Fix: convert-to-webp 경로 인코딩 로직 추가, 디버깅 로직 추가
    
    * Fix: 중복 인코딩 제거, vercel.json 단순화
    
    * Refactor: Cookie 관련 로직 util 함수로 통합 및 개선
    
    * Fix: 병합 충돌 해결, use-guest-mode.ts 삭제
    
    * Fix: 사이트 접속 초기 마이페이지로 리다이렉트되는 버그 수정
    
    * Fix: 프로필 데이터 요청 프롬프트 수정
    
    * Fix: 결과페이지 UI 수정
    
    * Fix: 결과페이지 백그라운드 작업 중 로딩 UI 추가, 비로그인의 경우 공유 버튼 노출 안되게 수정
    
    * Fix: 게스트모드 값 쿠키 설정 로직 명시적으로 예외처리

--- src/components/functional/landing-route.tsx
+++ src/components/functional/landing-route.tsx
@@ -8,11 +8,12 @@ export const LandingRoute = ({ children }: { children: React.ReactNode }) => {
     setCookie(COOKIE_NAMES.GUEST_MODE, true);
     return children;
   }
-
+  
   // 게스트 모드가 명시적으로 'false'인 경우 마이페이지로 리다이렉트
   if (isGuestMode === false) {
     return <Navigate to='/mypage' replace />;
   }
+
   // 게스트 모드가 true인 경우 랜딩 페이지 표시
   return children;
 };
