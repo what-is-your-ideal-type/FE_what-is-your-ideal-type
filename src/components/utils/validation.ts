f2ad66ef48e8e7f6bef217861461ac34566fa680
JheeKim <jheekim992@gmail.com>
2025-02-24 16:42:04 +0900

    Feat: 회원가입시 nickname 추가

--- src/components/utils/validation.ts
+++ src/components/utils/validation.ts
@@ -1,24 +1,25 @@
 import { z } from 'zod';
 
-export const emailSchema = z
-  .string()
-  .email('이메일 형식을 확인해주세요.')
+export const nicknameSchema = z.string().min(1, '닉네임을 입력해주세요.');
+
+export const emailSchema = z.string().email('이메일 형식을 확인해주세요.');
 
 export const passwordSchema = z
   .string()
   .min(6, '비밀번호는 6자 이상이어야 합니다.')
   .regex(
     /^[A-Za-z0-9]{6,}$/,
-    '비밀번호는 영문, 숫자를 혼합하여 6자 이상 입력해주세요.'
+    '비밀번호는 영문, 숫자를 혼합하여 6자 이상 입력해주세요.',
   );
 
 export const signUpSchema = z
   .object({
+    nickname: nicknameSchema,
     email: emailSchema,
     password: passwordSchema,
-    confirmPassword: passwordSchema
+    confirmPassword: passwordSchema,
   })
   .refine((data) => data.password === data.confirmPassword, {
     message: '비밀번호가 일치하지 않습니다.',
-    path: ['confirmPassword']
+    path: ['confirmPassword'],
   });
