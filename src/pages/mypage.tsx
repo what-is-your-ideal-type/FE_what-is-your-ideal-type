ba0d2752d1483b1cc9f107c4eb3aba70c38e7aa9
sennydayk <seyeon981217@gmail.com>
2025-02-26 16:51:18 +0900

    Fix: mypage 조건부 로직 추가

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { doc, getDoc } from 'firebase/firestore';
 import { useAuth } from '../contexts/auth-context';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import NavigateToSurvey from '../components/functional/navigate-to-survey-props';
 import { Text } from '../components/ui/text';
 import { FlexBox } from '../components/ui/flexbox';
 import { GridBox } from '../components/ui/gridbox';
 import { Card } from '../components/ui/card/card';
 import { Main } from '../components/ui/main';
+import { db } from '../firebase';
 import { Header } from '../components/ui/header';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { CardContent } from '../components/ui/card/card-content';
 import { CardDescription } from '../components/ui/card/card-description';
-import { User } from 'firebase/auth';
-import { fetchPostsData, fetchUserData } from '../api/queries';
 
 const MyPage = () => {
   const { currentUser } = useAuth();
@@ -20,44 +20,85 @@ const MyPage = () => {
   const navigate = useNavigate();
   const observer = useRef<IntersectionObserver | null>(null);
 
-  // 인증 상태 변경 감지
+  // 로그인되지 않은 경우 즉시 홈으로 이동
   useEffect(() => {
     if (!currentUser) {
       alert('로그인이 필요한 서비스입니다. 홈으로 이동합니다.');
+      navigate('/');
     }
-  }, [currentUser]);
+  }, [currentUser, navigate]);
 
-  // 쿼리 로직
-  const {
-    data: userData,
-    isLoading: isUserDataLoading,
-    error: userDataError,
-  } = useQuery({
-    queryKey: ['userData', currentUser?.uid],
-    queryFn: () => fetchUserData(currentUser as User),
-    enabled: !!currentUser,
-  });
+  const extractOccupation = (profileData: string) => {
+    try {
+      const profileObj = JSON.parse(profileData);
+      return profileObj.occupation || '직업 정보 없음';
+    } catch (error) {
+      console.error('프로필 파싱 중 오류 발생:', error);
+      return '직업 정보 없음';
+    }
+  };
 
-  const {
-    data: postsData,
-    isLoading: isPostsLoading,
-    error: postsError,
-    hasNextPage,
-    fetchNextPage,
-  } = useInfiniteQuery({
-    queryKey: ['posts', userData?.postList],
-    queryFn: ({ pageParam }) => fetchPostsData(userData, pageParam as number),
+  const getCardsData = async (pageParam: number) => {
+    try {
+      if (!currentUser) return { cards: [] }; // 불필요한 api 호출 방지
+
+      const uid = currentUser.uid;
+      const docRef = doc(db, 'users', uid);
+      const usersSnapShot = await getDoc(docRef);
+      const userData = usersSnapShot.data();
+
+      if (!userData || !userData.postList) return { cards: [] };
+
+      const postList = userData.postList.slice(
+        pageParam * 4,
+        pageParam * 4 + 4,
+      );
+
+      const fetchedCards = await Promise.all(
+        postList.map(async (num: string) => {
+          const postRef = doc(db, 'posts', num);
+          const postSnapShot = await getDoc(postRef);
+          const data = postSnapShot.data();
+
+          if (!data) return null;
+
+          return {
+            imageUrl: data.imageUrl,
+            createdAt: data.createdAt.toDate(),
+            fileName: data.fileName,
+            hashTags: data.hashTags,
+            resultUrl: `${window.location.href}/result/${data.id}`,
+            profile: data.profile,
+            id: data.id,
+          };
+        }),
+      );
+
+      return {
+        cards: fetchedCards.filter((card) => card !== null),
+        nextPage: fetchedCards.length === 4 ? pageParam + 1 : undefined,
+      };
+    } catch (error) {
+      console.error(error);
+      return { cards: [] };
+    }
+  };
+
+  const { data, isLoading, hasNextPage, fetchNextPage } = useInfiniteQuery({
+    queryKey: ['posts'],
+    queryFn: ({ pageParam }) => getCardsData(pageParam as number),
     initialPageParam: 0,
-    getNextPageParam: (lastPage) => {
-      return lastPage?.nextPage ?? null;
-    },
-    enabled: !!userData,
+    getNextPageParam: (lastPage) => lastPage?.nextPage ?? null,
+    enabled: !!currentUser, // 로그인한 경우 쿼리 로직 실행을 보장
   });
 
-  // Intersection Observer 설정
+  const cards = data?.pages.flatMap((page) => page?.cards) || [];
+
   useEffect(() => {
+    if (!hasNextPage) return;
+
     const loadMore = (entries: IntersectionObserverEntry[]) => {
-      if (entries[0].isIntersecting && hasNextPage) {
+      if (entries[0].isIntersecting) {
         fetchNextPage();
       }
     };
@@ -67,9 +108,9 @@ const MyPage = () => {
     }
 
     observer.current = new IntersectionObserver(loadMore, {
-      root: null,
+      root: null, // 뷰포트를 기준으로 함
       rootMargin: '0px',
-      threshold: 1.0,
+      threshold: 1.0, // 100% 가시성이 있어야 트리거
     });
 
     const target = document.getElementById('infinityQueryTrigger');
@@ -84,37 +125,19 @@ const MyPage = () => {
     };
   }, [hasNextPage, fetchNextPage]);
 
-  const extractOccupation = (profileData: string) => {
-    try {
-      const profileObj = JSON.parse(profileData);
-      return profileObj.occupation || '직업 정보 없음';
-    } catch (error) {
-      console.error('프로필 파싱 중 오류 발생:', error);
-      return '직업 정보 없음';
-    }
-  };
-
-  // 조건부 렌더링
-  if (!currentUser) {
-    return <Navigate to='/' replace />;
-  }
-
-  if (isUserDataLoading || isPostsLoading) {
+  if (isLoading) {
     return <div>로딩 중...</div>;
   }
-
-  if (userDataError || postsError) {
-    return <div>데이터를 불러오는 중 오류가 발생했습니다.</div>;
-  }
-
-  const posts = postsData?.pages.flatMap((page) => page?.posts) || [];
 
   return (
     <>
       <Header />
-      <FlexBox direction='column' className='bg-sub justify-center h-32 mb-8'>
+      <FlexBox
+        direction='column'
+        className='bg-gray-200 justify-center h-32 mb-8'
+      >
         <Text fontSize='xl' fontWeight='bold' className='py-4 text-center'>
-          {userData?.nickname}님의 마이페이지
+          {currentUser?.email}님의 마이페이지
         </Text>
       </FlexBox>
       <Main>
@@ -122,20 +145,20 @@ const MyPage = () => {
           <Text fontSize='xl' fontWeight='bold' className='text-center mb-8'>
             나의 이상형 리스트
           </Text>
-          {posts.length > 0 ? (
+          {cards.length > 0 ? (
             <GridBox>
-              {posts.map((post, index) => (
+              {cards.map((card, index) => (
                 <Card
                   key={index}
-                  onClick={() => navigate(`/result/${post.id}`)}
+                  onClick={() => navigate(`/result/${card.id}`)}
                 >
                   <CardContent
-                    imageUrl={post.imageUrl}
-                    altText={post.fileName}
+                    imageUrl={card.imageUrl}
+                    altText={card.fileName}
                     isImageLoaded={isImageLoaded}
                     onLoad={() => setIsImageLoaded(true)}
                   />
-                  <CardDescription title={extractOccupation(post.profile)} />
+                  <CardDescription title={extractOccupation(card.profile)} />
                 </Card>
               ))}
             </GridBox>
